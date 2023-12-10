d91af4cff31d97c110fd4d129916fa4f2fb4c859
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-12-10 14:49:56 -0300

    feat: adding the Footer component

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  padding: 1rem;
+  color: var(--primary);
+  font-weight: bold;
+  text-align: center;
+  background-color: var(--bg);
+  box-shadow: 0 -1rem 3rem 0 rgba(0, 0, 0, .25);
+`;
+
+export const Text = styled.p``;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Header } from "./Header";
+import { Title } from "./Title";
+import { Paragraph } from "./Paragraph";
+import { Footer } from "./Footer";
+
+export { Header, Title, Paragraph, Footer };

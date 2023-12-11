ab06063818822f2308a3bcff01ce4463ecedd5ef
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-12-11 17:31:23 -0300

    feat: adicionando página Signup ao Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {} from "./pages";
 
-import {About} from "./pages/About/index";
-import { EditPoint } from "./pages";
-import { DeleteUser } from "./pages";
-import { DeletePoint } from "./pages";
-
-import CreatePoint from "./pages/CreatePoint/index";
-import UpdateUser from "./pages/EditUser/index";
+import {
+  Signup,
+  About,
+  EditPoint,
+  EditUser,
+  DeleteUser,
+  CreatePoint,
+  DeletePoint
+} from "./pages";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/signup" element={<h1>Signup</h1>} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/user" element={<h1>User</h1>} />
-        <Route path="/user/edit" element={<UpdateUser/>} />
+        <Route path="/user/edit" element={<EditUser/>} />
         <Route path="/user/delete" element={<DeleteUser />} />
         <Route path="/map" element={<h1>Map</h1>} />
         <Route path="/point/create" element={<CreatePoint/>} />

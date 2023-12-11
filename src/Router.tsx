935a16e195cdb1cce09d1963774f69f69b2281ec
Jotave99 <joaobva2016@gmail.com>
2023-12-10 23:58:26 -0300

    Implement DeleteUser page

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import {} from "./pages";
 
 import {About} from "./pages/About/index";
 import CreatePoint from "./pages/CreatePoint/index";
+import { EditPoint } from "./pages";
+import { DeleteUser } from "./pages";
 
 export function Router() {
   return (
@@ -13,10 +15,10 @@ export function Router() {
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/user" element={<h1>User</h1>} />
         <Route path="/user/edit" element={<h1>Edit user</h1>} />
-        <Route path="/user/delete" element={<h1>Delete user</h1>} />
+        <Route path="/user/delete" element={<DeleteUser />} />
         <Route path="/map" element={<h1>Map</h1>} />
         <Route path="/point/create" element={<CreatePoint/>} />
-        <Route path="/point/edit" element={<h1>Edit point</h1>} />
+        <Route path="/point/edit" element={<EditPoint />} />
         <Route path="/point/delete" element={<h1>Delete point</h1>} />
         <Route path="/about" element={<About />} />
       </Routes>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,10 @@
 import { About } from "./About";
 import { EditPoint } from "./EditPoint";
+import { DeleteUser } from "./DeleteUser";
 
 export { 
     About, 
-    EditPoint
+    EditPoint,  
+    DeleteUser
 
 };

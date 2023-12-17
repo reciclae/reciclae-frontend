7be838830dee9ab87f8f5a5c95f1928915a581cf
Junior Silva <junior_silva158@hotmail.com>
2023-12-17 20:33:11 -0300

    Alterando Create point

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -4,12 +4,10 @@ import * as S from './style';
 import { useParams } from 'react-router-dom';
 import {Header} from '../../components/Header/index';
 import {Footer} from '../../components/Footer/index';
-import { z, ZodError } from 'zod';
 
 export const CreatePoint = () => {
   const { latitude, longitude } = useParams();
-  const token = localStorage.getItem("auth.token");
-  const user = localStorage.getItem("auth.user");
+  const user = JSON.parse(localStorage.getItem("auth.user") || "");
   const [formData, setFormData] = useState<{
     name: string;
     latitude: string;
@@ -35,43 +33,6 @@ export const CreatePoint = () => {
     image: null,
     user: '',
   });
-  // Schema do ZOD
-  const EcoPointSchema = z.object({
-    name: z.string().min(1),
-    latitude: z.string(),
-    longitude: z.string(),
-    metal: z.boolean(),
-    plastic: z.boolean(),
-    paper: z.boolean(),
-    glass: z.boolean(),
-    organic: z.boolean(),
-    electronic: z.boolean(),
-    user: z.string(),
-  });
-
-  // Validação personalizada para garantir que pelo menos um dos tipos seja true
-  EcoPointSchema.refine(data => {
-    const types: (keyof typeof data)[] =  ['metal', 'plastic', 'paper', 'glass', 'organic', 'electronic'];
-    return types.some(type => data[type]);
-  }, {
-    message: 'Pelo menos um tipo deve ser selecionado (metal, plastic, paper, glass, organic, electronic)',
-  });
-
-  // Função para validar os dados do formulário
-  const validateFormData = (data: any) => {
-    try {
-      // Tenta validar os dados usando o esquema
-      const validatedData = EcoPointSchema.parse(data);
-      return { valid: true, data: validatedData };
-    } catch (error) {
-      // Se ocorrer um erro, significa que os dados não são válidos
-      if (error instanceof ZodError) {
-        return { valid: false, errors: error.errors };
-      }
-      throw error;
-    }
-  };
-  
 
   const [imagePreview, setImagePreview] = useState<string | null>(null);
 
@@ -104,49 +65,37 @@ export const CreatePoint = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Valide os dados usando Zod
-    const validationResult = validateFormData(formData);
-  
-    if (validationResult.valid) {
-      const formDataWithImage = new FormData();
-      formDataWithImage.append('name', formData.name);
-      formDataWithImage.append('latitude', latitude ?? '');
-      formDataWithImage.append('longitude', longitude ?? '');
-      formDataWithImage.append('metal', String(formData.metal));
-      formDataWithImage.append('plastic', String(formData.plastic));
-      formDataWithImage.append('paper', String(formData.paper));
-      formDataWithImage.append('glass', String(formData.glass));
-      formDataWithImage.append('organic', String(formData.organic));
-      formDataWithImage.append('electronic', String(formData.electronic));
-      formDataWithImage.append('user', user ?? '');
-  
-    
-      if (formData.image !== null) {
-        formDataWithImage.append('image', formData.image);
+    const formDataWithImage = new FormData();
+    formDataWithImage.append('name', formData.name);
+    formDataWithImage.append('latitude', latitude ?? '');
+    formDataWithImage.append('longitude', longitude ?? '');
+    formDataWithImage.append('metal', String(formData.metal));
+    formDataWithImage.append('plastic', String(formData.plastic));
+    formDataWithImage.append('paper', String(formData.paper));
+    formDataWithImage.append('glass', String(formData.glass));
+    formDataWithImage.append('organic', String(formData.organic));
+    formDataWithImage.append('electronic', String(formData.electronic));
+    formDataWithImage.append('user', user.id ?? '');
+
+    if (formData.image !== null) {
+      formDataWithImage.append('image', formData.image);
+    }
+
+    try {
+      const response = await axios.post('http://localhost:3001/ecopoint', formDataWithImage, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      
+      if (response.data) {
+        alert('Eco ponto criado com sucesso!');
+      } else {
+        alert('Falha ao criar o eco ponto. Por favor, tente novamente.');
       }
-    
-      try {
-        const response = await axios.post('http://localhost:3001/ecopoint', formDataWithImage, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            // O token tem que vir abaixo.
-            'Authorization': 'Bearer '+ token
-          },
-        });
-        
-        if (response.data) {
-          alert('Eco ponto criado com sucesso!');
-        } else {
-          alert('Falha ao criar o eco ponto. Por favor, tente novamente.');
-        }
-      } catch (error) {
-        console.error('Erro ao fazer a requisição:', error);
-        alert('Erro ao criar o eco ponto. Por favor, tente novamente mais tarde.' + error);
-      }
-    } else {
-      // Os dados não são válidos
-      console.error('Erros de validação:', validationResult.errors);
-      alert('Os dados do formulário não são válidos. Corrija os erros antes de enviar.');
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error);
+      alert('Erro ao criar o eco ponto. Por favor, tente novamente mais tarde.' + error);
     }
   };
 

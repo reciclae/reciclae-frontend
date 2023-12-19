0aebcd5ddc60343c967537851dfbbb434a1a3aba
Junior Silva <junior_silva158@hotmail.com>
2023-12-19 03:23:41 -0300

    Adicionando animação de transição ao mudar de página

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Outlet, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./transition.css"; 
 
 import {
   Signup,
@@ -15,23 +18,35 @@ import {
 import { HomePage } from "./pages/HomePage";
 import { Map } from "./pages/Map/index";
 
+const AnimatedRoutes: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user" element={<UserProfile />} />
+          <Route path="/user/edit" element={<EditUser />} />
+          <Route path="/user/delete/:id" element={<DeleteUser />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/point/create/:latitude/:longitude" element={<CreatePoint />} />
+          <Route path="/point/select/:id" element={<SelectPoint />} />
+          <Route path="/point/edit/:id" element={<EditPoint />} />
+          <Route path="/point/delete/:id" element={<DeletePoint />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/user" element={<UserProfile />} />
-        <Route path="/user/edit" element={<EditUser/>} />
-        <Route path="/user/delete/:id" element={<DeleteUser />} />
-        <Route path="/map" element={<Map/>} />
-        <Route path="/point/create/:latitude/:longitude" element={<CreatePoint/>} />
-        <Route path="/point/select/:id" element={<SelectPoint />} />
-        <Route path="/point/edit/:id" element={<EditPoint />} />
-        <Route path="/point/delete/:id" element={<DeletePoint />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
-  )
+  );
 }
